4f1e6aed2b633fb8272ff57ae47bcebf79dfb441
Marco Cafasso <marcocafassog@gmail.com>
2022-11-16 14:02:36 +0100

    refactor: update Snack4.java

--- Snack4/src/org/generation/italy/Snack4.java
+++ Snack4/src/org/generation/italy/Snack4.java
@@ -6,7 +6,7 @@ public class Snack4 {
 
 	public static void main(String[] args) {
 		// Creare due array di dimensione diversa e stampare un numero di valori interi casuali pari alla differenza di dimensione 
-		// (es: arr1.length = 3; arr2.length = 10;  --> stampo 7 valori interi casuali)
+		// (es: arr1.length = 3; arr2.length = 10;  --> stampo 7 valori interi casuali) 
 		
 		int[] arr1 = new int[4];
 		int[] arr2 = new int [10];
